71fc77c7ae3762f9b7f0c8f81ff99fb059e98ee3
Paulo Felipe <p.felipe@msn.com>
2018-06-14 08:04:04 -0300

    Add ViewModel

---
+++ app/src/main/java/com/example/paulo/rsawithmvvm/criptografar/CriptografiaActivity.kt
@@ -0,0 +1,13 @@
+package com.example.paulo.rsawithmvvm.criptografar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.paulo.rsawithmvvm.R
+
+class CriptografiaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criptografia)
+    }
+}
