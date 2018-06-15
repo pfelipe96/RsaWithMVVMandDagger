6b1b15dca2a84c0c46121d230e0c1d0bc405aab4
Paulo Felipe <p.felipe@msn.com>
2018-06-15 08:55:23 -0300

    Add Layout and setting in the DataBinding

--- app/src/main/java/com/example/paulo/rsawithmvvm/criptografar/CriptografiaActivity.kt
+++ app/src/main/java/com/example/paulo/rsawithmvvm/criptografar/CriptografiaActivity.kt
@@ -1,13 +1,19 @@
 package com.example.paulo.rsawithmvvm.criptografar
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.paulo.rsawithmvvm.R
+import com.example.paulo.rsawithmvvm.databinding.ActivityCriptografiaBinding
 
 class CriptografiaActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityCriptografiaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_criptografia)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_criptografia)
+        binding.viewModel = CriptografiaViewModel()
+        binding.executePendingBindings()
     }
 }
