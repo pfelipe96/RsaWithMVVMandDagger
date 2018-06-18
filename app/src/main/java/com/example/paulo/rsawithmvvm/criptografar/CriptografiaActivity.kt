84ea9eb062920f9986210316e44009926f3a278d
Paulo Felipe <p.felipe@msn.com>
2018-06-17 23:48:40 -0300

    Implement style to app

--- app/src/main/java/com/example/paulo/rsawithmvvm/criptografar/CriptografiaActivity.kt
+++ app/src/main/java/com/example/paulo/rsawithmvvm/criptografar/CriptografiaActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.paulo.rsawithmvvm.R
 import com.example.paulo.rsawithmvvm.databinding.ActivityCriptografiaBinding
+import kotlinx.android.synthetic.main.toolbar.*
 
 class CriptografiaActivity : AppCompatActivity(), CriptografiaActivityInterface {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(toolbar_id)
+        toolbar_id.title = getString(R.string.action_bar_title_encryption)
+
         val criptografiaViewModel = CriptografiaViewModel(this)
         val binding = DataBindingUtil.setContentView<ActivityCriptografiaBinding>(this, R.layout.activity_criptografia)
         binding.viewModel = criptografiaViewModel
