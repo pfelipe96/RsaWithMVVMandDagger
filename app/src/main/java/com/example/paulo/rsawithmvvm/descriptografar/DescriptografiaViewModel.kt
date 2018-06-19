fde0b73d5171207a93e3ac9b4e98d89c9866a2e1
Paulo Felipe <p.felipe@msn.com>
2018-06-19 09:13:15 -0300

    Verify if is prime

--- app/src/main/java/com/example/paulo/rsawithmvvm/descriptografar/DescriptografiaViewModel.kt
+++ app/src/main/java/com/example/paulo/rsawithmvvm/descriptografar/DescriptografiaViewModel.kt
@@ -1,5 +1,59 @@
 package com.example.paulo.rsawithmvvm.descriptografar
 
+import android.databinding.ObservableField
+import android.text.Editable
+import android.text.TextWatcher
+
 class DescriptografiaViewModel{
 
+    val keyPublic = ObservableField<String>()
+    val functionTotiente = ObservableField<String>()
+
+    private var firstNumberPrime = 0
+    private var secondNumberPrime = 0
+
+    val watcherKeyPublic: TextWatcher = object: TextWatcher{
+        override fun afterTextChanged(s: Editable?) {
+            s?.let {
+                keyPublic.set(it.toString())
+            }
+
+            lookForKeyPrivate()
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+    }
+
+    val watcherFunctionTotiente: TextWatcher = object : TextWatcher{
+        override fun afterTextChanged(s: Editable?) {
+            s?.let {
+                functionTotiente.set(it.toString())
+            }
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+    }
+
+
+    private fun lookForKeyPrivate(){
+        var functionTotienteToInt = functionTotiente.get().toString().toInt()
+
+        for (i in 1..functionTotienteToInt){
+            if(functionTotienteToInt % i == 0){
+                var x = functionTotienteToInt / i
+                if((x+1) % 2 != 0 && (i+1) % 2 != 0) {
+
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/paulo/rsawithmvvm/mainActivity/MainViewHolder.kt
+++ app/src/main/java/com/example/paulo/rsawithmvvm/mainActivity/MainViewHolder.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.view.View
 import com.example.paulo.rsawithmvvm.R
 import com.example.paulo.rsawithmvvm.criptografar.CriptografiaActivity
+import com.example.paulo.rsawithmvvm.descriptografar.DescriptografiaActivty
 
 class MainViewHolder{
     fun onClick(view: View){
         when(view.id){
             R.id.send_encrypt ->  view.context.startActivity(Intent(view.context, CriptografiaActivity::class.java))
+            R.id.send_decrypt -> view.context.startActivity(Intent(view.context, DescriptografiaActivty::class.java))
         }
     }
 }
