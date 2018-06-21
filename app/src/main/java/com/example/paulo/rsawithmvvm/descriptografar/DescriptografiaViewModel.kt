87e1fcb6bf5eaa03ea69b5eea4a5afaf4a883b21
Paulo Felipe <p.felipe@msn.com>
2018-06-21 09:01:34 -0300

    Implement to verify if a number is prime

--- app/src/main/java/com/example/paulo/rsawithmvvm/descriptografar/DescriptografiaViewModel.kt
+++ app/src/main/java/com/example/paulo/rsawithmvvm/descriptografar/DescriptografiaViewModel.kt
@@ -27,8 +27,7 @@ class DescriptografiaViewModel{
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         }
     }
-
-    val watcherFunctionTotiente: TextWatcher = object : TextWatcher{
+    val watcherFunctionTotiente: TextWatcher = object: TextWatcher{
         override fun afterTextChanged(s: Editable?) {
             s?.let {
                 functionTotiente.set(it.toString())
@@ -43,17 +42,44 @@ class DescriptografiaViewModel{
 
     }
 
-
     private fun lookForKeyPrivate(){
-        var functionTotienteToInt = functionTotiente.get().toString().toInt()
+        val functionTotienteToInt = functionTotiente.get().toString().toInt()
+
+        var firstNumberPrimeLocal = 0
+        var secondNumberPrimeLocal = 0
 
         for (i in 1..functionTotienteToInt){
             if(functionTotienteToInt % i == 0){
-                var x = functionTotienteToInt / i
-                if((x+1) % 2 != 0 && (i+1) % 2 != 0) {
+                var y = i+1
+                var x = (functionTotienteToInt / i) + 1
+                if(x % 2 != 0 && y % 2 != 0) {
+                    for(z in 2 .. 9){
+                        if(x % z != 0){
+                            if(firstNumberPrimeLocal != -1) {
+                                firstNumberPrimeLocal = x
+                            }
+                        }else{
+                            firstNumberPrimeLocal = -1
+                        }
 
+                        if(y % z != 0){
+                            if(secondNumberPrimeLocal != -1) {
+                                secondNumberPrimeLocal = y
+                            }
+                        }else{
+                            secondNumberPrimeLocal = -1
+                        }
+                    }
+                    if(firstNumberPrimeLocal != -1 && secondNumberPrimeLocal != -1){
+                        firstNumberPrime = firstNumberPrimeLocal
+                        secondNumberPrime = secondNumberPrimeLocal
+                    }
+
+                    firstNumberPrimeLocal = 0
+                    secondNumberPrimeLocal = 0
                 }
             }
         }
     }
+
 }
