74ba62b889aa13f78f68c6f7c85452d4c6fc9200
Paulo Felipe <p.felipe@msn.com>
2018-06-22 08:10:49 -0300

    Fixed bug in decryption

--- app/src/main/java/com/example/paulo/rsawithmvvm/descriptografar/DescriptografiaViewModel.kt
+++ app/src/main/java/com/example/paulo/rsawithmvvm/descriptografar/DescriptografiaViewModel.kt
@@ -11,8 +11,7 @@ class DescriptografiaViewModel{
     val functionTotiente = ObservableField<String>()
     val textFirstNumberPrime = ObservableField<String>()
     val textSecondNumberPrime = ObservableField<String>()
-    val linearLayoutNumbersPrime = ObservableField<Boolean>(false)
-
+//    val linearLayoutNumbersPrime = ObservableField<Boolean>(false)
     private lateinit var timer: Timer
 
 //    val watcherKeyPublic: TextWatcher = object: TextWatcher{
@@ -38,7 +37,7 @@ class DescriptografiaViewModel{
             timer = Timer()
             timer.schedule(object : TimerTask() {
                 override fun run() {
-                    s?.let {
+                    s.let {
                         lookForKeyPrivate()
                     }
                 }
@@ -50,54 +49,55 @@ class DescriptografiaViewModel{
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            if (timer != null) {
-                timer.cancel()
+            try {
+                timer.let { timer.cancel() }
+            }catch (exception: Exception){
+                exception.printStackTrace()
             }
         }
     }
 
 
-    private fun lookForKeyPrivate(){
-        val functionTotienteToInt = functionTotiente.get().toString().toInt()
+    private fun lookForKeyPrivate() {
+        if (functionTotiente.get().toString().isNullOrEmpty()) {
+            val functionTotienteToInt = functionTotiente.get().toString().toInt()
 
-        var firstNumberPrimeLocal = 0
-        var secondNumberPrimeLocal = 0
+            var firstNumberPrimeLocal = 0
+            var secondNumberPrimeLocal = 0
 
-        for (i in 1..functionTotienteToInt){
-            if(functionTotienteToInt % i == 0){
-                var y = i+1
-                var x = (functionTotienteToInt / i) + 1
-                if(x % 2 != 0 && y % 2 != 0) {
-                    for(z in 2 .. 9){
-                        if(x % z != 0){
-                            if(firstNumberPrimeLocal != -1) {
-                                firstNumberPrimeLocal = x
+            for (i in 1..functionTotienteToInt) {
+                if (functionTotienteToInt % i == 0) {
+                    var y = i + 1
+                    var x = (functionTotienteToInt / i) + 1
+                    if (x % 2 != 0 && y % 2 != 0) {
+                        for (z in 2..9) {
+                            if (x % z != 0) {
+                                if (firstNumberPrimeLocal != -1) {
+                                    firstNumberPrimeLocal = x
+                                }
+                            } else {
+                                firstNumberPrimeLocal = -1
                             }
-                        }else{
-                            firstNumberPrimeLocal = -1
+
+                            if (y % z != 0) {
+                                if (secondNumberPrimeLocal != -1) {
+                                    secondNumberPrimeLocal = y
+                                }
+                            } else {
+                                secondNumberPrimeLocal = -1
+                            }
+                        }
+                        if (firstNumberPrimeLocal != -1 && secondNumberPrimeLocal != -1) {
+                            textFirstNumberPrime.set(firstNumberPrimeLocal.toString())
+                            textSecondNumberPrime.set(secondNumberPrimeLocal.toString())
+//                        linearLayoutNumbersPrime.set(true)
                         }
 
-                        if(y % z != 0){
-                            if(secondNumberPrimeLocal != -1) {
-                                secondNumberPrimeLocal = y
-                            }
-                        }else{
-                            secondNumberPrimeLocal = -1
-                        }
+                        firstNumberPrimeLocal = 0
+                        secondNumberPrimeLocal = 0
                     }
-                    if(firstNumberPrimeLocal != -1 && secondNumberPrimeLocal != -1){
-                        textFirstNumberPrime.set(firstNumberPrimeLocal.toString())
-                        textSecondNumberPrime.set(secondNumberPrimeLocal.toString())
-                        linearLayoutNumbersPrime.set(true)
-                    }
-
-                    firstNumberPrimeLocal = 0
-                    secondNumberPrimeLocal = 0
                 }
             }
         }
     }
-
-
-
 }
